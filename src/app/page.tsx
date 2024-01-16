74de8746e5e8e4bc5660d9e5b23b52de5f02bc5f
Leonardo Barros <leonprogramer@gmail.com>
2024-01-16 15:17:22 -0300

    Numbers of steps added in section how to transfer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,13 +100,18 @@ export default function Home() {
             <Suspense fallback={<Loading />}>
               <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 mt-10 mb-20">
                 {stepsForTransfer.map((content, index) => (
-                  <div key={index} className="card-from-step">
+                  <div key={index} className="card-from-step relative">
                     <Image
                       src={content.image}
                       alt={content.description}
-                      className="mx-auto"
+                      className="mx-auto stroke-0 md:stroke-2"
                     />
-                    <h4 className="text-lg md:text-xl lg:text-2xl text-center text-danger leading-[120%] px-2.5 mt-4 mb-8">{content.msg}</h4>
+                    <h4 className="text-lg md:text-xl lg:text-2xl text-center text-danger leading-[120%] px-2.5 mt-4 mb-8">
+                      {content.msg}
+                    </h4>
+                    <span className="absolute left-2 md:left-6 top-2 md:top-6 h-6 md:h-8 w-6 md:w-8 rounded-2xl bg-danger text-sm md:text-base lg:text-lg md:text-xl text-center text-blackBlue text-white font-bold subpixel-antialiased">
+                      {index + 1}
+                    </span>
                   </div>
                 ))}
               </section>
